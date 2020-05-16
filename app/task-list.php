10a290b32964caec3c3dd63b8ac6e9db231c17bd
Barbara Siegrist <siegrist.barbara@gmail.com>
2020-05-16 11:22:35 +0200

    list all tasks with duedate and buttons - no styles

--- app/task-list.php
+++ app/task-list.php
@@ -1,10 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasklist Challenge – WebProfessionals</title>
+</head>
+<body>
+    <main>
+        <h1>Taskliste</h1>
 
-    require_once("init.php");
+        <div class="tasklist">
+            <?php
 
-    $taskLoader = new TaskLoader();
+                require_once("init.php");
 
-    echo "<h2>Alle ausgeben</h2>";
-    print_array($taskLoader->getAll());
+                //hole alle tasks aus der DB als Array
+                $taskLoader = new TaskLoader();
+                $alltasks = $taskLoader->getAll();
 
-?>
+                foreach ($alltasks as $task){
+                    $tasktitle = $task['title'];
+                    $taskduedate = $task['duedate'];
+                    echo "<div class='tasklist__taskline'><h3 class='tasklist_tasktitle'>$tasktitle</h3><p>$taskduedate</p><button class='tasklist_taskdetails'>Details</button><button class='tasklist_edit'>Bearbeiten</button><button class='tasklist_delete'>Löschen</button></div>";
+                }
+
+                
+            ?>
+        </div>
+    </main>
+</body>
+</html>
+
